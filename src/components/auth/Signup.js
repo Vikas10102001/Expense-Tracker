d3c87a313798ec8e212bb2e8a46e33ba699367e8
Vikas <provikas2001@gmail.com>
2023-04-16 23:16:07 +0530

    Added validation logic for signup

--- src/components/auth/Signup.js
+++ src/components/auth/Signup.js
@@ -1,32 +1,119 @@
-import React from "react";
+import React, { useEffect, useReducer, useState } from "react";
 import { Link } from "react-router-dom";
 import CustomInput from "./customInput/CustomInput";
 import AuthBox from "../ui/AuthBox";
+import validateEmail from "../../utils/emailValidator";
+import validatePassword from "../../utils/passwordValidator";
 
 export default function Signup() {
-  const handleEmailOnChange = () => {};
-  const handlePasswordOnChange = () => {};
-  const handleConfirmPasswordOnChange = () => {};
+  const [formIsValid, setFormIsValid] = useState(false);
+  const [invalidMessage, setInvalidMessage] = useState("");
+  const [enteredEmail, dispatchEmail] = useReducer(
+    (state, action) => {
+      if (action.type === "USER_INPUT") {
+        return { val: action.value, isValid: validateEmail(action.value) };
+      }
+      if (action.type === "INPUT_VALIDITY") {
+        return { val: state.val, isValid: validateEmail(state.val) };
+      }
+    },
+    { val: "", isValid: null }
+  );
+  const [enteredPassword, dispatchPassword] = useReducer(
+    (state, action) => {
+      if (action.type === "USER_INPUT") {
+        return { val: action.value, isValid: validatePassword(action.value) };
+      }
+      if (action.type === "INPUT_VALIDITY") {
+        return { val: state.val, isValid: validatePassword(state.val) };
+      }
+    },
+    { val: "", isValid: null }
+  );
+  const [enteredConfirmPassword, dispatchConfirmPassword] = useReducer(
+    (state, action) => {
+      if (action.type === "USER_INPUT") {
+        return {
+          val: action.value,
+          isValid:
+            validatePassword(action.value) &&
+            enteredPassword.val === action.value,
+        };
+      }
+      if (action.type === "INPUT_VALIDITY") {
+        return {
+          val: state.val,
+          isValid:
+            validatePassword(state.val) && enteredPassword.val === state.val,
+        };
+      }
+    },
+    { val: "", isValid: null }
+  );
+  const { isValid: emailValidity } = enteredEmail;
+  const { isValid: passwordValidity } = enteredPassword;
+  const { isValid: confirmPasswordValidity } = enteredConfirmPassword;
+  useEffect(() => {
+    if (emailValidity === false) {
+      setInvalidMessage("Please enter valid email");
+    } else if (passwordValidity == false) {
+      setInvalidMessage("Password must contain atleast 8 character");
+    } else if (confirmPasswordValidity === false) {
+      setInvalidMessage("Password does not match!");
+    }
+    setFormIsValid(
+      emailValidity && passwordValidity && confirmPasswordValidity
+    );
+  }, [emailValidity, passwordValidity, confirmPasswordValidity]);
+
+  const handleEmailOnChange = (e) => {
+    dispatchEmail({ type: "USER_INPUT", value: e.target.value });
+  };
+  const handleValidateEmail = (e) => {
+    dispatchEmail({ type: "INPUT_VALIDITY" });
+  };
+  const handlePasswordOnChange = (e) => {
+    dispatchPassword({ type: "USER_INPUT", value: e.target.value });
+  };
+  const handleValidatePassword = () => {
+    dispatchPassword({ type: "INPUT_VALIDITY" });
+  };
+  const handleConfirmPasswordOnChange = (e) => {
+    dispatchConfirmPassword({ type: "USER_INPUT", value: e.target.value });
+  };
+  const handleValidateConfirmPassword = () => {
+    dispatchConfirmPassword({ type: "INPUT_VALIDITY" });
+  };
   return (
     <AuthBox>
       <CustomInput
         type="text"
         id="email"
         label="Email"
+        value={enteredEmail.val}
+        isValid={enteredEmail.isValid}
         onChange={handleEmailOnChange}
+        onBlur={handleValidateEmail}
       />
       <CustomInput
         type="password"
         id="password"
         label="Password"
+        value={enteredPassword.val}
+        isValid={enteredPassword.isValid}
         onChange={handlePasswordOnChange}
+        onBlur={handleValidatePassword}
       />
       <CustomInput
         type="password"
         id="confirm-password"
         label="Confirm password"
         onChange={handleConfirmPasswordOnChange}
+        onBlur={handleValidateConfirmPassword}
+        isValid={enteredConfirmPassword.isValid}
+        value={enteredConfirmPassword.val}
       />
+      <div className="invalid-message">{invalidMessage}</div>
       <button className="login-signup-logout-button">Signup</button>
       <div className="redirect-link">
         <Link to="/login">Already have an account?</Link>
--- src/components/auth/customInput/CustomInput.js
+++ src/components/auth/customInput/CustomInput.js
@@ -1,10 +1,18 @@
 import React from "react";
 import "./CustomInput.css";
-export default function CustomInput({ label, id, value, onChange, type }) {
+export default function CustomInput({
+  label,
+  id,
+  value,
+  onChange,
+  type,
+  isValid,
+  onBlur,
+}) {
   return (
-    <div className="input-container">
+    <div className={`input-container ${isValid === false ? "invalid" : ""}`}>
       <label htmlFor={id}>{label}</label>
-      <input type={type} onChange={onChange} value={value} />
+      <input type={type} onChange={onChange} value={value} onBlur={onBlur} />
     </div>
   );
 }
