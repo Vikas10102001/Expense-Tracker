72de6fb664050aba7c8b399a12d444a7c1414f78
Vikas <provikas2001@gmail.com>
2023-04-16 19:50:37 +0530

    Created routes

--- src/App.js
+++ src/App.js
@@ -1,63 +1,20 @@
-import Expenses from "./components/expense/Expenses";
-import AddExpense from "./components/new-expense/AddExpense"
-import "./components/expense/Expenses.css"
-import Card from "./components/ui/Card"
-import { useState } from "react";
+import NavBar from "./components/navigation/NavBar";
+import Login from "./components/auth/Login";
+import { Route, Routes } from "react-router-dom";
+import Signup from "./components/auth/Signup";
+import Home from "./components/home/Home";
 
-let InitialExpense = [{
-  "id": 'asdada21',
-  "date": new Date(2021, 6, 5),
-  "title": 'Car Insurance',
-  "amount": 206
-},
-{
-  "id": 'ads1',
-  "date": new Date(2021, 7, 15),
-  "title": 'Car Service',
-  "amount": 149
-},
-{
-  "id": 'asads66',
-  "date": new Date(2021, 8, 5),
-  "title": 'New phone',
-  "amount": 1120
-},
-{
-  "id": 'jhhsd91',
-  "date": new Date(2021, 5, 30),
-  "title": 'share',
-  "amount": 1800
-}]
 function App() {
-  const [expense, setExpenses] = useState(InitialExpense);
-  const [filteredYear, setFilteredYear] = useState('2022')
-  const newExpenseHandler = (ExpenseItem) => {
-    const newExpenseItem = {
-      id: Math.random().toString(),
-      ...ExpenseItem,
-    }
-    //  console.log(newExpenseItem)
-    setExpenses((prevState) => {
-      return [
-        newExpenseItem,
-        ...prevState
-      ]
-    })
-    // console.log(expense)
-  }
-
-  const filteredExpense = expense.filter((item) => {
-    return item.date.getFullYear().toString() === filteredYear
-  }
-  )
-
   return (
-    <div className="container">
-      <Card className="additems">
-        <AddExpense onGettingNewExpense={newExpenseHandler} />
-      </Card>
-      <Expenses items={filteredExpense} selectedYear={filteredYear} setYear={setFilteredYear} />
-    </div>
+    <>
+      <NavBar />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/home" element={<Home/>} />
+      </Routes>
+    </>
   );
 }
 
