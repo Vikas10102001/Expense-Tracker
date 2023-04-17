cc6d327ba5bc7f88ef71f135418360f73bfb3572
Vikas <provikas2001@gmail.com>
2023-04-17 15:51:23 +0530

    exported auth

--- src/firebase.js
+++ src/firebase.js
@@ -20,5 +20,5 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
+export const auth = getAuth(app);
 const analytics = getAnalytics(app);
