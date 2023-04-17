d4c6827fcb3b27769e3859c09f60ea3a7e7c4d63
Vikas <provikas2001@gmail.com>
2023-04-17 23:34:58 +0530

    navigate to home if user is logged in or to login page if not

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import NavBar from "./components/navigation/NavBar";
 import Login from "./components/auth/Login";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import Signup from "./components/auth/Signup";
 import Home from "./components/home/Home";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 function App() {
+  const navigate = useNavigate();
+  const auth = getAuth();
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      console.log(user);
+      navigate("/home");
+      const uid = user.uid;
+    } else {
+      navigate("/login");
+    }
+  });
   return (
     <>
       <NavBar />
@@ -12,7 +24,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/home" element={<Home/>} />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </>
   );
