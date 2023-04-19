a30ef5356b5b761bfa5ca7299ce8f6b8831400b9
Vikas <provikas2001@gmail.com>
2023-04-19 20:38:45 +0530

    updated key for expense item

--- src/components/home/expense/Expenses.js
+++ src/components/home/expense/Expenses.js
@@ -1,18 +1,26 @@
 import FilterExpense from "./FilterExpense";
-import ExpenseItems from "./ExpenseItems"
-import ExpenseChart from "../chart/ExpenseChart"
-import Card from "../../ui/Card"
-import "./Expenses.css"
+import ExpenseItems from "./ExpenseItems";
+import ExpenseChart from "../chart/ExpenseChart";
+import Card from "../../ui/Card";
+import "./Expenses.css";
 const Expenses = (props) => {
-    // console.log(props.items)
+  // console.log(props.items)
 
-
-    return (
-        <Card className="items ">
-            <FilterExpense selectedYear={props.selectedYear} setYear={props.setYear} />
-            <ExpenseChart items={props.items} />
-            {props.items.length === 0 ? <p className="no_expense">No Expenses</p> : props.items.map((expense) => <ExpenseItems item={expense} key={expense.id}></ExpenseItems>)}
-        </Card>
-    )
-}
-export default Expenses
+  return (
+    <Card className="items ">
+      <FilterExpense
+        selectedYear={props.selectedYear}
+        setYear={props.setYear}
+      />
+      <ExpenseChart items={props.items} />
+      {props.items.length === 0 ? (
+        <p className="no_expense">No Expenses</p>
+      ) : (
+        props.items.map((expense, ind) => (
+          <ExpenseItems item={expense} key={ind}></ExpenseItems>
+        ))
+      )}
+    </Card>
+  );
+};
+export default Expenses;
