8e22624107c1fdd88e246f42591fbd7c24ff5003
Vikas <provikas2001@gmail.com>
2023-04-19 20:36:34 +0530

    updated filter menu

--- src/components/home/expense/FilterExpense.js
+++ src/components/home/expense/FilterExpense.js
@@ -8,10 +8,10 @@ const FilterExpense=(props)=>{
         <div className="dropdown">
             <label className="filter_label">Filter By Year</label>
             <select value={props.selectedYear} name="year" id="filter" onChange={onFilterChange}>
-                <option value="2019">2019</option>
-                <option value="2020">2020</option>
-                <option value="2021">2021</option>
-                <option value="2022">2022</option>
+                <option value="2023">2023</option>
+                <option value="2024">2024</option>
+                <option value="2025">2025</option>
+                <option value="2026">2026</option>
             </select>
         </div>
     )
