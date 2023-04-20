7793d0bb35560609a4c4d2593308e38c4709a1fe
Vikas <provikas2001@gmail.com>
2023-04-20 18:48:22 +0530

    Added alert reducer

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authSlice from "./slice/auth";
+import alertSlice from "./slice/alert";
 
 const store = configureStore({
   reducer: {
     auth: authSlice.reducer,
+    alert: alertSlice.reducer,
   },
 });
 
