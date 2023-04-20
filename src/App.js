d45eadf14c3792808f71db383e2374a47366c487
Vikas <provikas2001@gmail.com>
2023-04-20 14:33:39 +0530

    Added spinner

--- src/App.js
+++ src/App.js
@@ -5,8 +5,13 @@ import Signup from "./components/auth/Signup";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { useEffect } from "react";
 import HomePage from "./components/home/HomePage";
+import { useSelector } from "react-redux";
+import { RotatingLines } from "react-loader-spinner";
 
 function App() {
+  const isLoading = useSelector((state) => {
+    return state.auth.isLoading;
+  });
   const navigate = useNavigate();
   const auth = getAuth();
   useEffect(() => {
@@ -27,6 +32,11 @@ function App() {
   return (
     <>
       <NavBar />
+      {isLoading && (
+        <div className="spinner">
+          <RotatingLines strokeColor="white" height={50} width={100} />
+        </div>
+      )}
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/" element={<Login />} />
