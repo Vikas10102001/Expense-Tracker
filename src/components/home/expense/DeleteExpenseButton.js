b61e772e9745c9373872730a7ad426ea4301aab1
Vikas <provikas2001@gmail.com>
2023-04-21 23:18:50 +0530

    Deleting expense from database

--- src/components/home/expense/DeleteExpenseButton.js
+++ src/components/home/expense/DeleteExpenseButton.js
@@ -1,9 +1,22 @@
 import React from "react";
 import { FaTimesCircle } from "react-icons/fa";
 import "./DeleteExpenseButton.css";
-export default function DeleteExpenseButton({expense}) {
-    const handleDeleteExpense=()=>{
-        console.log(expense)
-    }
-  return <FaTimesCircle className="deleteButton"  onClick={handleDeleteExpense}/>;
+import { getDatabase, ref, remove } from "firebase/database";
+export default function DeleteExpenseButton({ expense }) {
+  const handleDeleteExpense = () => {
+    const expenseId = expense.key;
+    const user=JSON.parse(localStorage.getItem("user"))
+    const db = getDatabase();
+    const expenseRef = ref(db, "expenses/" + user.uid + "/" + expenseId);
+    remove(expenseRef)
+      .then(() => {
+        console.log("Expense deleted successfully");
+      })
+      .catch((error) => {
+        console.error("Error deleting expense:", error);
+      });
+  };
+  return (
+    <FaTimesCircle className="deleteButton" onClick={handleDeleteExpense} />
+  );
 }
