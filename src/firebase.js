23666b7168b4e53e0fcf0fe323e836cdb7d36af5
Vikas <provikas2001@gmail.com>
2023-04-23 21:26:41 +0530

    accessing configuration data from .env

--- src/firebase.js
+++ src/firebase.js
@@ -7,14 +7,14 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDkPtTeoLXtNgRU6nbY2lVgYJKTr-t1w0Q",
-  authDomain: "expense-tracker-acd77.firebaseapp.com",
-  databaseURL: "https://expense-tracker-acd77-default-rtdb.firebaseio.com",
-  projectId: "expense-tracker-acd77",
-  storageBucket: "expense-tracker-acd77.appspot.com",
-  messagingSenderId: "182777440794",
-  appId: "1:182777440794:web:75cc728115f0aa91937fa3",
-  measurementId: "G-G190LZFWR5",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
