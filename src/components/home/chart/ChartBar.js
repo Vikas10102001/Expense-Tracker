9e736d88e7b6f4477aff6f0ea9993eea79b05144
Vikas <provikas2001@gmail.com>
2023-04-24 21:03:26 +0530

    Added label for showing total expenses for a month

--- src/components/home/chart/ChartBar.js
+++ src/components/home/chart/ChartBar.js
@@ -1,16 +1,17 @@
-import "./ChartBar.css"
+import "./ChartBar.css";
 const ChartBar = (props) => {
-    let barFillHeight = '0%';
-    if (props.value > 0)
-        barFillHeight = Math.round((props.value / props.maxVal) * 100 )+ "%";
-    return (
-        <div className="bar_container">
-            <div className="bar">
-                <div className="bar_fill" style={{height:barFillHeight}}></div>
-            </div>
-            <div className="bar_label">{props.label}</div>
-        </div>
-    )
-}
+  let barFillHeight = "0%";
+  if (props.value > 0)
+    barFillHeight = Math.round((props.value / props.maxVal) * 100) + "%";
+  return (
+    <div className="bar_container">
+      <div className="bar_label">{props.value}</div>
+      <div className="bar">
+        <div className="bar_fill" style={{ height: barFillHeight }}></div>
+      </div>
+      <div className="bar_label">{props.label}</div>
+    </div>
+  );
+};
 
-export default ChartBar
+export default ChartBar;
